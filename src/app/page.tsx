ee90485cd73502ef17c0450e268310fb9b8d719b
Shot287 <atoyhs.1102@gmail.com>
2025-12-02 22:49:06 +0900

    feat: ○○機能を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ const DOCS = [
   { docKey: "mental_vas_v1", localKey: "mental_vas_v1" },
   { docKey: "mental_defusion_v1", localKey: "mental_defusion_v1" },
   { docKey: "mental_loving_kindness_v1",  localKey: "loving_kindness_v1" },
-  { docKey: "math_logic_expansion_v1", localKey: "math_logic_expansion_v1" },
+  // { docKey: "math_logic_expansion_v1", localKey: "math_logic_expansion_v1" },
   { docKey: "study_sapuri_words_v1", localKey: "study_sapuri_words_v1" },
   { docKey: "process_goals_v1", localKey: "process_goals_v1" },
   { docKey: "reflection_note_v1", localKey: "reflection_note_v1" },
