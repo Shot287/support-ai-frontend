86c0dff8c441907fb35aa24ce9c77f9f205ca37e
Shot287 <atoyhs.1102@gmail.com>
2026-02-05 20:33:41 +0900

    feat: ○○機能を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ const DOCS = [
   { docKey: "process_goals_v1", localKey: "process_goals_v1" },
   //{ docKey: "reflection_note_v1", localKey: "reflection_note_v1" },
   //{ docKey: "math_formulas_v1", localKey: "math_formulas_v1" },
-  { docKey: "checklist_v1", localKey: "checklist_v1" },
+  //{ docKey: "checklist_v1", localKey: "checklist_v1" },
   { docKey: "mental_emotion_labeling_v1", localKey: "emotion_labeling_v1" },
   //{ docKey: "math_dictionary_v1", localKey: "math_dictionary_v1" },
   //{ docKey: "study_lisp_dictionary_v1", localKey: "lisp_dictionary_v1" },
